e8f179d764e5af825c78d624984f19297a461c6e
Andrew H <heider.andrew@gmail.com>
2012-01-21 13:43:33 -0800

    Andy is still a bitch

---
+++ SS12Game/SS12Game/SS12Game/Vocal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Research.Kinect.Audio;
+using Microsoft.Speech.AudioFormat;
+using Microsoft.Speech.Recognition;
+
+namespace SS12Game
+{
+    class Vocal
+    {
+
+        private KinectAudioSource kinectSource;
+        private SpeechRecognitionEngine sre;
+        private const string RecognizerId = "SR_MS_en-US_Kinect_10.0";
+        private bool paused = false;
+        private bool valid = false;
+
+        public bool IsValid()
+        {
+            return valid;
+        }
+
+        private void StartDMO()
+        {
+            kinectSource = new KinectAudioSource();
+            kinectSource.SystemMode = SystemMode.OptibeamArrayOnly;
+            kinectSource.FeatureMode = true;
+            kinectSource.AutomaticGainControl = false;
+            kinectSource.MicArrayMode = MicArrayMode.MicArrayAdaptiveBeam;
+            var kinectStream = kinectSource.Start();
+            sre.SetInputToAudioStream(kinectStream, new SpeechAudioFormatInfo(
+                                                  EncodingFormat.Pcm, 16000, 16, 1,
+                                                  32000, 2, null));
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        public void Stop()
+        {
+            if (sre != null)
+            {
+                sre.RecognizeAsyncCancel();
+                sre.RecognizeAsyncStop();
+                kinectSource.Dispose();
+            }
+        }
+
+    }
+}
